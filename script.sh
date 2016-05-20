d9ffe777f206aa7dff0e8ba37b09023fb3d4c864
jerodcle <jeroen.declerck@ugent.be>
2016-05-20 23:49:38 +0200

    v3.1

--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@ DB=/home/jeroen/Documents/Github/CHCat/db.csv
 
 rm $DB
 touch $DB
-#for file in $(find htmlfiles/belgian-cigars/full-boxes-4/ -name '*.html')
-for file in $(find htmlfiles/ -name '*.html')
+for file in $(find htmlfiles/belgian-cigars/full-boxes-4/ -name '*.html')
+#for file in $(find htmlfiles/ -name '*.html')
 do
   echo "$file"
 
@@ -20,7 +20,8 @@ do
   rm $file"tmp.txt"
 
   #if boxamount is present, put boxamount in its field, if not, set it to "1?"
-  sed -i -e 's/,\([0-9]\+\)€;/.\1€\n/g' -e 's/\(.*;.*\)\(;.*\)/\1;1\2/g' -e 's/\(.*\) \/\([0-9]\+.*\);1/\1;\2/g' $file".txt"
+  sed -i 's/,\([0-9]\+\)€;/.\1€\n/g' $file".txt" # split line after every price
+  sed -i -e 's/\(.*;.*\)\(;.*\)/\1;1?\2/g' -e 's/\(.*\) \/\([0-9]\+.*\);1?/\1;\2/g' -e 's/;1?;/;1;/g' $file".txt" #if boxamount present, put boxamount in its field, if its not present, set it to 1.
 
   #add categories, remove timestamps and other junk
   IFS='/' read -ra ADDR <<< "$file"
@@ -28,7 +29,13 @@ do
   sed -i -e "s/$/;${ADDR[1]}/g" -e "s/$/;${ADDR[2]}/g" -e "s/$/;${ADDR[3]}/g" \
   -e "s/[0-9]\+-[0-9]\+-[0-9]\+-[0-9]\+-[0-9]\+-[0-9]\+//g" -e "s/-[0-9]\+;/;/g" $file".txt"
 
+  #get price per cigar
+  while IFS=';' read c1 c2 c3 c4 c5 c6
+  do
+    echo "scale=2; x=${c4::-1}/$c3; if(x<1) print 0; x" | bc -l
+  done < $file".txt"
+
   #write to db and cleanup
   cat $file".txt" >> "$DB"
-  rm $file".txt"
+  #rm $file".txt"
 done
