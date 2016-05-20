1157f652d882af6eed926d59388df8d2a52d103d
jerodcle <jeroen.declerck@ugent.be>
2016-05-20 20:59:54 +0200

    v2

--- script.sh
+++ script.sh
@@ -8,11 +8,13 @@ for file in $(find htmlfiles/ -name '*.html')
 do
   echo "$file"
   grep 'PricesalesPriceWithDiscount\|browseProductImage' $file > $file"tmp.txt"
-  sed -i 's/.*>\([0-9]\+,[0-9]\+\) €.*/\1€/g' $file"tmp.txt"
-  sed -i 's/.*src="\(.*\.jpg\)".*title="\(.*\)\ \/\([0-9]\+\)"  \/><\/a>/\1;\2;\3/g' $file"tmp.txt"
-  tr '\n' ';' < $file"tmp.txt" > $file".txt"
+  sed -i 's/.*>\([0-9]\+,[0-9]\+\) €.*/\1€/g' $file"tmp.txt" #get price
+  sed -i 's/.*src="\(.*\.\(jpg\|png\|gif\)\)".*title="\(.*\)"  \/><\/a>/\1;\3/g' $file"tmp.txt" #get title and box
+  tr '\n' ';' < $file"tmp.txt" > $file".txt" # get everything on one line
   rm $file"tmp.txt"
-  sed -i 's/€;/€\n/g' $file".txt"
+  sed -i 's/€;/€\n/g' $file".txt" # split line after every price
+  sed -i 's/\(.*;.*\)\(;.*\)/\1;1?\2/g' $file".txt" # split line after every price
+  sed -i 's/\(.*\) \/\([0-9]\+.*\);1?/\1;\2/g' $file".txt" #if boxamount present, put boxamount in its field
   cat $file".txt" >> "$DB"
-  rm $file".txt"
+  #rm $file".txt"
 done
