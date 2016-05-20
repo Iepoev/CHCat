077e02b764007766687bb482f27574bd98362558
jerodcle <jeroen.declerck@ugent.be>
2016-05-20 23:16:39 +0200

    v3

--- script.sh
+++ script.sh
@@ -4,17 +4,31 @@ DB=/home/jeroen/Documents/Github/CHCat/db.csv
 
 rm $DB
 touch $DB
+#for file in $(find htmlfiles/belgian-cigars/full-boxes-4/ -name '*.html')
 for file in $(find htmlfiles/ -name '*.html')
 do
   echo "$file"
+
+  #get raw html data
   grep 'PricesalesPriceWithDiscount\|browseProductImage' $file > $file"tmp.txt"
-  sed -i 's/.*>\([0-9]\+,[0-9]\+\) €.*/\1€/g' $file"tmp.txt" #get price
-  sed -i 's/.*src="\(.*\.\(jpg\|png\|gif\)\)".*title="\(.*\)"  \/><\/a>/\1;\3/g' $file"tmp.txt" #get title and box
-  tr '\n' ';' < $file"tmp.txt" > $file".txt" # get everything on one line
+
+  #parse primary data
+  sed -i -e 's/.*>\([0-9]\+,[0-9]\+\) €.*/\1€/g' -e 's/.*src="\(.*\.\(jpg\|png\|gif\)\)".*title="\(.*\)"  \/><\/a>/\1;\3/g' $file"tmp.txt"
+
+  # set everything on one line
+  tr '\n' ';' < $file"tmp.txt" > $file".txt"
   rm $file"tmp.txt"
-  sed -i 's/€;/€\n/g' $file".txt" # split line after every price
-  sed -i 's/\(.*;.*\)\(;.*\)/\1;1?\2/g' $file".txt" # split line after every price
-  sed -i 's/\(.*\) \/\([0-9]\+.*\);1?/\1;\2/g' $file".txt" #if boxamount present, put boxamount in its field
+
+  #if boxamount is present, put boxamount in its field, if not, set it to "1?"
+  sed -i -e 's/,\([0-9]\+\)€;/.\1€\n/g' -e 's/\(.*;.*\)\(;.*\)/\1;1\2/g' -e 's/\(.*\) \/\([0-9]\+.*\);1/\1;\2/g' $file".txt"
+
+  #add categories, remove timestamps and other junk
+  IFS='/' read -ra ADDR <<< "$file"
+  unset ADDR[${#ADDR[@]}-1]
+  sed -i -e "s/$/;${ADDR[1]}/g" -e "s/$/;${ADDR[2]}/g" -e "s/$/;${ADDR[3]}/g" \
+  -e "s/[0-9]\+-[0-9]\+-[0-9]\+-[0-9]\+-[0-9]\+-[0-9]\+//g" -e "s/-[0-9]\+;/;/g" $file".txt"
+
+  #write to db and cleanup
   cat $file".txt" >> "$DB"
-  #rm $file".txt"
+  rm $file".txt"
 done
