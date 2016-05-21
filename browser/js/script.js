4780dc25f764b6cf8ce0338cf8a8358d37e53394
jerodcle <jeroen.declerck@ugent.be>
2016-05-21 20:23:27 +0200

    moar cleanup to table

--- browser/js/script.js
+++ browser/js/script.js
@@ -3,6 +3,11 @@ var config = {
   base_path: 'js/tablefilter/',
   alternate_rows: true,
 
+  col_widths: [
+    '200px', '500px', '50px', '50px',
+    '100px', '100px', '100px', '100px',
+  ],
+
   col_number_format: [
     null, null, null,'EU',
     null, null, null, null
