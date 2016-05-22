b01a9abd276feab5cae22e628113a13a92c0a3c1
jerodcle <jeroen.declerck@ugent.be>
2016-05-22 13:12:14 +0200

    remove duplicate files

--- browser/js/script.js
+++ browser/js/script.js
@@ -2,17 +2,22 @@
 var config = {
   base_path: 'js/tablefilter/',
   alternate_rows: true,
+  highlight_keywords: true,
+  auto_filter: true,
+  auto_filter_delay: 500,
+  btn_reset: true,
+  status_bar: true,
+  msg_filter: 'Filtering...',
 
   col_widths: [
-    '200px', '500px', '50px', '50px',
-    '100px', '100px', '100px', '100px',
-  ],
-
-  col_number_format: [
-    null, null, null,'EU',
+    null, '500px', '50px', '50px',
     null, null, null, null
   ],
 
+  col_number_format: [
+    null, null, 'number', 'number',
+    'EU', null, null, null
+  ],
   extensions: [{ name: 'sort' }]
 };
 
